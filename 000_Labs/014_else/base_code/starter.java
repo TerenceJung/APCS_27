98f80ace87a66931800e25b65bfc226742c34bf4
TerenceJung <jungterence890@gmail.com>
2026-09-22 19:47:52 +0000

    update

--- 000_Labs/014_else/base_code/starter.java
+++ 000_Labs/014_else/base_code/starter.java
@@ -1,6 +1,6 @@
 /*
- *	Author:  
- *  Date: 
+ *	Author:  Terence Jung
+ *  Date: 9/21/2026
 */
 
 import java.util.Scanner;
@@ -10,6 +10,9 @@ class starter {
 	public static void main(String args[]) {
 		// the string "I love to learn coding remotely." will appear in
 		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely."); 
+		
+
+
+		
 	}
 }
